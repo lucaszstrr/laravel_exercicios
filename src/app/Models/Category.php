98e52cd67660f48e3dd434f642adc3ba853c2372
lucaszstrr <lucaaszstr@gmail.com>
2025-03-17 09:29:26 -0300

    Model criada

--- src/app/Models/Category.php
+++ src/app/Models/Category.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'categotyName',
+        'description'
+    ];
+
+    public function products(){
+        return $this->hasMany(Product::class);
+    }
+    
 }
