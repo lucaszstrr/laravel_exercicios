f9dad0540a2974f894631fd2662ee335752163a7
lucaszstrr <lucaaszstr@gmail.com>
2025-03-17 11:48:19 -0300

    Rotas

--- src/routes/api.php
+++ src/routes/api.php
@@ -14,6 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/teste', function(){
+    return "Funcionou";
+});
+
+Route::get('/products', [ProductController::class, 'index']);
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
