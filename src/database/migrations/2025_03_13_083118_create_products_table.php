dc2083ef820cc12e23eac7bbd1587276761eb9db
lucaszstrr <lucaaszstr@gmail.com>
2025-03-17 08:55:31 -0300

    Colunas adicionadas

--- src/database/migrations/2025_03_13_083118_create_products_table.php
+++ src/database/migrations/2025_03_13_083118_create_products_table.php
@@ -14,6 +14,8 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->foreignId('id')->references('id')->on('categories');
             $table->string('name');
+            $table->float('price');
+            $table->string('description');
             $table->timestamps();
         });
     }
